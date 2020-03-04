205b298a35d7c1e8a74833c31ae51ec666f12e23
Bálint Séra <balint.sera@gmail.com>
2020-03-04 08:08:30 +0100

    Adding wrapper for react php http middleware

--- README.md
+++ README.md
@@ -23,13 +23,20 @@ and the same in PHP:
 $app = new \Exphpress\App();
 $port = 8080;
 $app->get('/hello', function (ServerRequestInterface $request) {
+    error_log("handler", 4);
     return new Response(
         200,
         array(
             'Content-Type' => 'text/plain'
         ),
-        'hello world'
+        'Hello World!'
     );
 });
+
+$app->use(function (ServerRequestInterface $request, callable $next) {
+    error_log("example middleware");
+    return $next($request);
+});
+
 $app->listen($port);
 ```
--- server.php
+++ server.php
@@ -4,19 +4,21 @@ use \Psr\Http\Message\ServerRequestInterface;
 use React\Http\Response;
 
 $app = new \Exphpress\App();
+$port = 8080;
 $app->get('/hello', function (ServerRequestInterface $request) {
     error_log("handler", 4);
-     new Response(
+    return new Response(
         200,
         array(
             'Content-Type' => 'text/plain'
         ),
-        'hello world'
+        'Hello World!'
     );
 });
+
 $app->use(function (ServerRequestInterface $request, callable $next) {
-    error_log("first middleware");
-    $next($request);
+    error_log("example middleware");
+    return $next($request);
 });
 
-$app->listen(8080);
+$app->listen($port);
